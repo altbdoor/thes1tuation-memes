9e1107a004f4d9894c3755716b7646590097ce9a
altbdoor <lancersupraskyline@gmail.com>
2025-09-07 01:06:04 +0800

    try using podman

--- run.sh
+++ run.sh
@@ -4,15 +4,15 @@ set -e
 
 IMAGE_NAME="jekyll-dev:latest"
 
-if [[ "$1" == "rebuild" || "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
-    docker build --progress plain -t $IMAGE_NAME .
+if [[ "$1" == "rebuild" || "$(podman images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
+    podman build --progress plain -t $IMAGE_NAME .
 fi
 
 if [[ "$1" == "sh" ]]; then
-    docker run --rm -it -v "$PWD:/srv" $IMAGE_NAME sh
+    podman run --rm -it -v "$PWD:/srv" $IMAGE_NAME sh
 elif [[ "$1" == "build" ]]; then
-    docker run --rm -it -v "$PWD:/srv" $IMAGE_NAME bundle exec jekyll build
+    podman run --rm -it -v "$PWD:/srv" $IMAGE_NAME bundle exec jekyll build
 else
-    docker run --rm -it -p '4000:4000' -p '35729:35729' -v "$PWD:/srv" $IMAGE_NAME \
+    podman run --rm -it -p '4000:4000' -p '35729:35729' -v "$PWD:/srv" $IMAGE_NAME \
         bundle exec jekyll serve --livereload --host 0.0.0.0 --force_polling
 fi
